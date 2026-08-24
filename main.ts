6ac49d56bdf4a918cc8e810ba55a723e857e2789
Dudu-Moura <eduardo.moura2007@hotmail.com>
2026-08-24 17:08:22 -0300

    feat: Including image uploader endpoint

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
 import express, { type Express, type Request, type Response} from 'express'
+import multer from 'multer';
 
+const upload = multer({ dest: '.uploads/'})
 const app: Express = express();
 const port = 3000
 
+
+type ImageStatus = 'PENDING' | 'PROCESSING' | 'COMPLETED' | 'FAILED'
+
 app.get('/health', (req: Request, res: Response) => {
     res.send({ status: 'ok' });
+})
+
+app.post('/photo', upload.single('photo'),(req: Request, res: Response) => {
+    console.log(req.file);
+    res.json({ file: req.file?.filename });
 })
 
 app.listen(port);
