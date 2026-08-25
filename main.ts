5c615db7a2d89ca25e291118b176621867e8f0ac
Dudu-Moura <eduardo.moura2007@hotmail.com>
2026-08-25 15:59:04 -0300

    feat: adding exhibition image endpoint

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 import express, { type Express, type Request, type Response} from 'express'
 import multer from 'multer';
+import fs from 'node:fs' 
 
 const upload = multer({ dest: '.uploads/'})
 const app: Express = express();
 const port = 3000
-
 
 type ImageStatus = 'PENDING' | 'PROCESSING' | 'COMPLETED' | 'FAILED'
 
@@ -16,5 +16,12 @@ app.post('/photo', upload.single('photo'),(req: Request, res: Response) => {
     console.log(req.file);
     res.json({ file: req.file?.filename });
 })
+
+app.get('/photo', (req: Request, res: Response) => {
+    const photo = fs.readFileSync('.uploads/9236b836b0fc0e431f561987a18e9d59');
+    res.type('jpeg')
+    res.send(photo);
+})
+
 
 app.listen(port);
