721567b35c5bf514eb7c900e3f3d86206343e543
Dudu-Moura <eduardo.moura2007@hotmail.com>
2026-09-08 10:30:58 -0300

    feat: adding fileFilter to accept only image uploading

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import { instance } from './db/instance.db.js';
 import { photos } from './db/schema.js';
 
 
-const upload = multer({ dest: '.uploads/'})
+
+const upload = multer({ dest: '.uploads/', fileFilter: (_, file, cb) => {
+    if(file.mimetype.substring(0,5) !== 'image'){
+        cb(new Error('File type not accepted'));
+    }
+    else{
+        cb(null, true);
+    }
+} })
+
 const app: Express = express();
 const port = 3000
 
@@ -15,23 +24,24 @@ app.get('/health', (_: Request, res: Response) => {
     res.send({ status: 'ok' });
 })
 
-app.post('/photo', upload.single('photo'),async (req: Request, res: Response) => {
+app.post('/photo', upload.single('photo'), async (req: Request, res: Response) => {
     console.log(req.file);
 
-   try{ await instance.database
-    .insert(photos)
-    .values({
-        originalName: req.file!.originalname,
-        fileName: req.file!.filename,
-        mimeType: req.file!.mimetype,
-        size: req.file!.size,
-        path: req.file!.path,
-    });}
-    catch(error) {
-        console.error(error);
-    }
+        try{ await instance.database
+            .insert(photos)
+            .values({
+                originalName: req.file!.originalname,
+                fileName: req.file!.filename,
+                mimeType: req.file!.mimetype,
+                size: req.file!.size,
+                path: req.file!.path,
+            });
+        }
+        catch(error) {
+                console.error(error);
+        }
 
-    res.json({ file: req.file?.filename });
+        res.json({ file: req.file?.filename });
 });
 
 app.get('/photo', (_: Request, res: Response) => {
