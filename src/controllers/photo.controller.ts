f79c7388274cec1935ebee6981b3eaba05ed988a
Dudu-Moura <eduardo.moura2007@hotmail.com>
2026-09-14 21:17:38 -0300

    feat: adding miniature format and adding directory layer for each image upload - organization layer

--- src/controllers/photo.controller.ts
+++ src/controllers/photo.controller.ts
@@ -1,35 +1,36 @@
 import type { Request, Response, NextFunction } from "express";
 import type { PhotoService } from "../service/photo.service.js";
-import type { Photo } from "../types/photo.types.js";
-import fs, { readFileSync } from 'node:fs';
+import fs, { readFile } from 'node:fs/promises';
 import exifr from 'exifr'
-import path from "node:path";
+import sharp from 'sharp'
+import path from 'node:path'
+
 
 export class PhotoController {
     constructor(private photoService: PhotoService){};
 
-    getPhoto = async (req: Request, res: Response, next: NextFunction) => {
+    getPhoto = async (_: Request, res: Response) => {
         const photos = await this.photoService.getPhotos();
 
         const metadatas = await Promise.all(photos.map(p => exifr.parse(`${p.path}`)));
 
-        const readFiles = photos.map(p => readFileSync(p.path));
+        const readFiles = await Promise.all(photos.map(p => readFile(p.path)));
         res.status(200).json({
-            photos: readFiles.map(rF => rF.toString('base64')),
+            photos: readFiles.map( rF => rF.toString('base64')),
             metadatas: metadatas,
             mimeType: photos.map(p => p.mimeType)
         })
 
     }
 
-    getPhotoById = async (req: Request, res: Response, _: NextFunction) => {
+    getPhotoById = async (req: Request, res: Response) => {
         const id = String(req.params.id);
         const photo = await this.photoService.getPhotoById(id);
 
         const metadata = await exifr.parse(`.uploads/${photo.fileName}`);
         console.log(metadata);
 
-        const readPhoto = fs.readFileSync(photo.path);
+        const readPhoto = await fs.readFile(photo.path);
 
         res.status(200).json({
             photo: readPhoto.toString('base64'), 
@@ -38,7 +39,7 @@ export class PhotoController {
         });
     }
 
-    createPhoto = async (req: Request, res: Response, _: NextFunction) => {
+    createPhoto = async (req: Request, res: Response) => {
         const file = req.file!;
         const photo = {
             path: file.path,
@@ -48,12 +49,30 @@ export class PhotoController {
             size: file.size,
         };
 
+        const miniatureFormat = await sharp(`${photo.path}`)
+        .resize({
+            width: 300,
+            height: 300 
+        })
+        .toFormat('webp')
+        .webp({
+            quality: 80,
+            nearLossless: true
+        })
+        .toBuffer();
+
+        const absolutePath = path.join(`/app`, `/.uploads/${photo.originalName.substring(0, photo.originalName.indexOf('.'))}`);
+
+        await fs.mkdir(absolutePath);
+        await fs.writeFile(`${absolutePath}/${photo.originalName}` , miniatureFormat, 'base64');
+
+        await fs.rename(photo.path, `${absolutePath}/${photo.originalName}1`);
         await this.photoService.createPhoto(photo);
 
         res.status(201).json({ message: `Image created -  ${JSON.stringify(photo)}`});
     }
 
-    createMultiplePhotos = async (req: Request, res: Response, _: NextFunction) => {
+    createMultiplePhotos = async (req: Request, res: Response) => {
         const files = req.files as Express.Multer.File[];
         const photos = files.map(f => ({
             path: f.path,
